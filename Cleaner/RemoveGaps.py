f459589b9fcd03dca1947468844e4b487abda062
lbundalian <rosencrantz01@gmail.com>
2021-11-16 15:38:34 +0100

    Update RemoveGaps

--- Cleaner/RemoveGaps.py
+++ Cleaner/RemoveGaps.py
@@ -6,6 +6,8 @@ Date : 09092021
 Description : Script to remove the part of the sequence which are not conserved across 
 the defined species
 
+Last Modified : 05102021
+
 """
 
 # =============================================================================
@@ -15,7 +17,9 @@ the defined species
 import Bio
 import os
 import sys
-
+import re
+import numpy as np
+import logging
 
 from Bio import AlignIO
 from Bio import SeqIO
@@ -25,26 +29,18 @@ from collections import Counter
 from Bio.Align import MultipleSeqAlignment
 from collections import Counter
 
+from numpy.core.fromnumeric import shape
+from numpy.core.numeric import normalize_axis_tuple
+from numpy.lib.function_base import average
 
 
-#filename = sys.argv[1]
 
-#parameters = sys.argv
-
-#with open(filename) as file:
-#	data = []
-#	for line in file:
-#		data.append(line[:-1])
-
-#threshold = data[0]
-#gap = data[1]
-#align = data[2]
 
 threshold = sys.argv[1]
 gap = sys.argv[2]
-align = sys.argv[3]
+concensus = sys.argv[3]
+align1 = sys.argv[4]
 species = []
-
 
 
 # =============================================================================
@@ -53,29 +49,25 @@ species = []
 #   aln - the multiple sequence alignment object to be trimmed 
 #   thr - the % threshold considered for removing the columns with gaps; this will
 #         determine the maximum allowable portion of gaps 
-#         (range from 0-100)
 # Description :
 # A method used to removed sites with gaps among the species of a given multiple
+#         (range from 0-100)
 # sequence alignment object.
 # =============================================================================
 
-
-def remove_gap(aln, thr):
+def remove_gap(aln,state_cols):
     
-    clean_aln = []
     keep_cols = []
-    
-    thr = thr/100
-    
-    for i in range(aln.get_alignment_length()):
+    clean_aln = []
+
+    for i in range(0,len(state_cols),3):
+        k = 0
+        for j in range(3):
+            k+=state_cols[i+j]
         
-        col_nucs = [sr.seq[i].upper() for sr in aln]
-        counter = Counter(col_nucs)
-
-        if counter['-']/len(col_nucs) >= thr :
-            continue
-
-        keep_cols.append(i)
+        if k == 0:
+            for ctr in range(i,i+3):
+                keep_cols.append(ctr)
 
     for s in aln:
         sequence = '%s\n' % ''.join([s.seq[i] for i in keep_cols])
@@ -86,8 +78,137 @@ def remove_gap(aln, thr):
                         description = s.description)
         clean_aln.append(seq)
     
-    return MultipleSeqAlignment(clean_aln)
+    return MultipleSeqAlignment(clean_aln)    
 
+# =============================================================================
+# Method Name : mask_sequence
+# Parameters :
+#   aln - the multiple sequence alignment object to be trimmed 
+#   n - the number of maximum neighbors to mask
+# Description :
+# A method used to mask n consecutive gaps in a sequence
+# =============================================================================
+
+def mask_sequence(aln):
+    
+    _sequence = []
+
+    for a in aln:
+        
+        nuc = str(a.seq)
+        
+        _nucleotides = re.finditer('-{10,}', nuc)
+        for n in _nucleotides:
+            pos = n.start()
+            l = n.end()-n.start()
+            nuc = nuc[:pos] + 'X'*l + nuc[pos+l:]	 
+            
+        nuc = nuc.replace('-','N')
+        nuc = nuc.replace('X','-')
+        _seq = SeqRecord(Seq(nuc),
+                        id = a.id,
+                        name = a.name,
+                        description = a.description)
+        
+        _sequence.append(_seq)
+    
+    return(MultipleSeqAlignment(_sequence))
+        
+
+# =============================================================================
+# Method Name : create_state_matrix
+# Parameters :
+#   aln - the multiple sequence alignment object to be trimmed 
+#   thr - the threshold at which certain block will be flag for discard or keep
+# Description :
+# A method generate a matrix containing flags for discarding or keeping a block
+# =============================================================================
+
+def create_state_matrix(aln, thr):
+
+    clean_aln = []
+    state_cols = []
+    
+    thr = thr/100
+    
+    for i in range(aln.get_alignment_length()):
+        
+        col_nucs = [sr.seq[i].upper() for sr in aln]
+        counter = Counter(col_nucs)
+        # if greater than threshold, discard [1] else keep [0]
+        if counter['-']/len(col_nucs) >= thr :
+            state_cols.append(1)
+        else:
+            state_cols.append(0)    
+
+    
+    return state_cols
+
+# =============================================================================
+# Method Name : create_concensus
+# Parameters :
+#   aln - the multiple sequence alignment object from which concesus of nucleo-
+#   tides will be created 
+# Description :
+# A method generate a matrix containing concensus for each blocks
+# =============================================================================
+
+def create_concensus(aln):
+
+    concensus = []
+    
+    
+    for i in range(aln.get_alignment_length()):
+        
+        col_nucs = [sr.seq[i].upper() for sr in aln]
+        counter = Counter(col_nucs)
+        concensus.append(dict(counter))
+    
+    return concensus
+
+# =============================================================================
+# Method Name : match_concensus
+# Parameters :
+#   aln - the multiple sequence alignment object from to concesus of nucleo-
+#   tides will be matched
+#   concensus -  the amount of each nucleotides in a block in batches
+#   thr -  the allowable % portion of the sequence which can be considered as 
+#   non conforming nucleotide across a block
+# Description :
+# A method remove non conforming sequence
+# =============================================================================
+
+def match_concensus(concensus,aln, thr = 10):
+
+    _sequence = []
+    seq_concensus = []
+    thr = thr/100
+
+    for a in aln:
+        for _a in range(len(a.seq)):
+            
+            # and the portion at which the nucleotide constitute should be larger than the average or larger than 20%
+
+            if concensus[_a][a.seq[_a]] > concensus[_a][min(concensus[_a], key = concensus[_a].get)]:
+                # good
+                seq_concensus.append(1)
+            else :
+                # bad
+                seq_concensus.append(0)
+    
+        ctr = dict(Counter(seq_concensus))
+        per_ctr = ctr[0]/sum(ctr.values())
+        if( per_ctr < thr):
+            _seq = SeqRecord(Seq(a.seq),
+                        id = a.id,
+                        name = a.name,
+                        description = a.description)
+        
+        
+            _sequence.append(_seq)
+        else:
+            logging.info("Too high percent mismatch in concensus in {0} - {1:.4g}%".format(a.name,per_ctr*100))
+    return(MultipleSeqAlignment(_sequence))
 
 # =============================================================================
 # Method Name : percent_gap
@@ -105,19 +226,159 @@ def percent_gap(align,gapthr):
     for a in align:
         nuc_count = Counter(a)
         pergap = (nuc_count['-']/len(a)) * 100  
-        if gapthr < pergap:
-            species.append(a.id)
-    return(species)
+        if pergap < gapthr:
+            _seq = SeqRecord(Seq(a.seq),
+                        id = a.id,
+                        name = a.name,
+                        description = a.description)
+        
+            species.append(_seq)
+        else:
+            logging.info("Too much gaps for {0}".format(a.name))
+    return(MultipleSeqAlignment(species))
+
+# =============================================================================
+# Method Name : find_frameshifts
+# Parameters :
+#   aln - the multiple sequence alignment object to be trimmed 
+# Description :
+# A method to find sequences with frameshifts i.e num_gaps%3 != 0
+# =============================================================================
+
+def find_frameshifts(aln,ln = 3,min = 0):
+    species = []
+    for a in aln:
+        nuc = str(a.seq)
+        nuc = nuc.replace('N','-')
+        matches = re.finditer('-{1,}',nuc)
+        mod3 = []
+        
+        for match in matches:
+            mod3.append(len(match.group())%ln)
+
+        if (Counter(mod3)[1] + Counter(mod3)[2]) == min:
+            _seq = SeqRecord(Seq(nuc),
+                        id = a.id,
+                        name = a.name,
+                        description = a.description)
+            species.append(_seq)
+        else:
+            logging.info("Frameshift detected in {0}".format(a.name))
+    return(MultipleSeqAlignment(species))
 
 
+
+# =============================================================================
+# Method Name : matrix_transform
+# Parameters :
+#   aln - transform the matrix into an array of 3mers 
+# Description :
+# A method generate a matrix containing flags for discarding or keeping a block
+# =============================================================================
+def matrix_transform(aln, n = 3):
+    msa_matrix = []
+    for a in aln:
+        _str_aln = str(a.seq)
+        str_aln = [_str_aln[i:i+n] for i in range(0, len(_str_aln), n)] 
+        msa_matrix.append(str_aln)
+    return(np.vstack(msa_matrix))
+
+
+# =============================================================================
+# Method Name : search_stop_codon
+# Parameters :
+#   aln - alignment to be observed
+# Description :
+# A method generate to search for premature stop codon
+# =============================================================================
+def search_stop_codon(aln, n = 3):
+            
+        sequences = []
+
+        stop_codons = ['TGA','TAA','TAG']
+
+        for _alignment in aln:
+            
+            premature = False
+            #print(_alignment.name)
+            
+            for index in range(0, len(_alignment.seq), 3):
+                codon = _alignment.seq[index:index+3]
+                if codon in stop_codons and index < (len(_alignment.seq)/3):
+                    print("STOPPED")
+                    logging.info("Premature stop codon in {0} Block : {1}".format(_alignment.name, index))
+                    premature = True
+                    break
+
+            
+
+            if not premature:
+                
+                _seq = SeqRecord(Seq(_alignment.seq),
+                                    id = _alignment.id,
+                                    name = _alignment.name,
+                                    description = _alignment.description)
+                
+        
+                sequences.append(_seq)
+      
+            else:
+                
+                continue
+
+        return(MultipleSeqAlignment(sequences))
+        
+        
 
 directory = os.getcwd()
 
 if __name__=='__main__':
-    alignments = AlignIO.read("{0}\\{1}".format(directory,align), "fasta")
-    clean_alignments = remove_gap(alignments, float(threshold))
-    print(clean_alignments)
-    percent_gap(clean_alignments,float(gap))
-    print(species)
-    os.system('cmd /k {0} {1}'.format('samtool faidx'," ".join(species)))
+    
+    logging.basicConfig(filename='events.log',level=logging.INFO)
+
+    debug = False
+
+    # read the alignment
+    alignments = AlignIO.read("{0}/{1}".format(directory,align1), "fasta")
+        
+
+    if not debug: 
+        
+        # mask the gaps with bp length < 10
+        mask_alignments = mask_sequence(alignments)
+        
+        # assign if the flags for each blocks [Discard or Keep]
+        state_matrix = create_state_matrix(mask_alignments, float(threshold))
+        
+        # remove the gaps and keep the ones without DISCARD flag - 0
+        clean_alignments = remove_gap(mask_alignments,state_matrix)
+        
+        # remove species with %gap
+        no_gap_sequences = percent_gap(clean_alignments,float(gap))
+        
+        # remove gaps which is not divisible by 3
+        no_frameshift_sequences = find_frameshifts(no_gap_sequences)   
+        
+        # check for block wise agreement for the alignments
+        # for checking
+        con = create_concensus(no_frameshift_sequences)
+        matched_matrix = match_concensus(con,no_frameshift_sequences, int(concensus))
+        
+        no_premature_matrix = search_stop_codon(matched_matrix)
+        
+        file_handle = open("output.fasta","w")
+
+        #AlignIO.write(no_premature_matrix, file_handle, "fasta")
+
+        file_handle.close()
+
+        #print(no_premature_matrix)
+
+        for _s in no_premature_matrix:
+            #print(_s.seq)
+            print(">{0}".format(_s.id))
+            print("{0}".format(_s.seq), end = '')
+
+        
+
 
