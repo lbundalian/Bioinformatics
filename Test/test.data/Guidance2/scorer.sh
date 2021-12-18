410b66bf6d8301aaf4173f86fc61c5789a923f6f
lbundalian <rosencrantz01@gmail.com>
2021-12-18 21:31:56 +0100

    Added guidance2 scores

--- Test/test.data/Guidance2/scorer.sh
+++ Test/test.data/Guidance2/scorer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 for i in {1..12}
 do
- echo D$i >> Scores.txt
- python AlignmentScore.py -i d${i}_guidance.fasta >> Scores.txt
- echo "Done D$i"
- cd ..
+echo D$i >> Scores.txt
+python AlignmentScore.py -i d${i}_guidance.fasta >> Scores.txt
+echo "Done D$i"
 done
